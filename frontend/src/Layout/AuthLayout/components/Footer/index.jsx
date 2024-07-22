fd65398d8fcc2311fd2e450ddc3436d1945a7626
LeilaEerfanirad <leila.erfanirad@gmail.com>
2024-07-22 21:31:52 +0330

    test git

--- frontend/src/Layout/AuthLayout/components/Footer/index.jsx
+++ frontend/src/Layout/AuthLayout/components/Footer/index.jsx
@@ -4,6 +4,7 @@ export default function Footer() {
         <footer className="w-full bg-blue-700 text-center py-7">
             <p className="text-white font-semibold">
                 تمام حقوق مادی این وبسایت متعلق به دانشگاه شهید بهشتی تهران میباشد
+                تمام حقوق مادی این وبسایت متعلق به دانشگاه شهید بهشتی تهران میباشد
             </p>
         </footer>
     );
